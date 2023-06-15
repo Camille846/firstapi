5891cdf7dd1dce5b1212c560217c2450dd0a735a
Camille <83260908+Camille846@users.noreply.github.com>
2023-06-15 20:54:01 -0300

    Feat: Criar users.js

---
+++ src/mocks/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Ana Silva',
+    },
+    {
+        id: 2,
+        name: 'Pedro Albuquerque',
+    },
+    {
+        id: 3,
+        name: 'Maria Pereira',
+    },
+    {
+        id: 4,
+        name: 'João Souza',
+    },
+    {
+        id: 5,
+        name: 'Paulo Machado',
+    }
+]
