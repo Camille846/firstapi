50378861b3ce9cea42356595b286204d454f49b9
Camille <83260908+Camille846@users.noreply.github.com>
2023-06-21 20:38:36 -0300

    Feat: Using query params and adding sortedUsers function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,7 +2,20 @@ const users = require('../mocks/users');
 
 module.exports = { 
     listUsers: (request, response) => {
+        // pegar a query string da url e o destruturing para pegar o parametro order 
+        const { order } = request.query
+        // criar função para ordenar os usuarios e se o order for desc, ordenar de forma decrescente, se não, ordenar de forma crescente
+        const sortedUsers = users.sort((a,b) => {
+            if(order === 'desc') {
+                return a.id < b.id ? 1 : -1;
+            }
+
+            return a.id > b.id ? 1 : -1;
+        })
+
+
         response.writeHead(200, { 'Content-Type' : 'application/json' })
-        response.end(JSON.stringify(users))
+        // Mostrar o array de usuarios ordenado
+        response.end(JSON.stringify(sortedUsers))
     },
 }
